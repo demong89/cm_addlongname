ff23e0034697e7bb5a60c40eacc82cdd5272874c
guoxingwang <wb-gxw02154864@alibaba-inc.com>
2022-08-22 17:07:00 +0800

    feat:  version

--- src/.cz-config.js
+++ src/.cz-config.js
@@ -17,23 +17,23 @@ module.exports = {
   ],
 
   // scope 类型（定义之后，可通过上下键选择）
-  scopes: [
-    ['components', '组件相关'],
-    ['hooks', 'hook 相关'],
-    ['utils', 'utils 相关'],
-    ['element-ui', '对 element-ui 的调整'],
-    ['styles', '样式相关'],
-    ['deps', '项目依赖'],
-    ['auth', '对 auth 修改'],
-    ['other', '其他修改'],
-    // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
-    ['custom', '以上都不是？我要自定义']
-  ].map(([value, description]) => {
-    return {
-      value,
-      name: `${value.padEnd(30)} (${description})`
-    }
-  }),
+  // scopes: [
+  //   ['components', '组件相关'],
+  //   ['hooks', 'hook 相关'],
+  //   ['utils', 'utils 相关'],
+  //   ['element-ui', '对 element-ui 的调整'],
+  //   ['styles', '样式相关'],
+  //   ['deps', '项目依赖'],
+  //   ['auth', '对 auth 修改'],
+  //   ['other', '其他修改'],
+  //   // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
+  //   ['custom', '以上都不是？我要自定义']
+  // ].map(([value, description]) => {
+  //   return {
+  //     value,
+  //     name: `${value.padEnd(30)} (${description})`
+  //   }
+  // }),
 
   // 是否允许自定义填写 scope，在 scope 选择的时候，会有 empty 和 custom 可以选择。
   // allowCustomScopes: true,
@@ -58,9 +58,9 @@ module.exports = {
   // 交互提示信息
   messages: {
     type: '确保本次提交遵循：前端代码规范！\n选择你要提交的类型：',
-    scope: '\n选择一个 scope（可选）：',
+    // scope: '\n选择一个 scope（可选）：',
     // 选择 scope: custom 时会出下面的提示
-    customScope: '请输入自定义的 scope：',
+    // customScope: '请输入自定义的 scope：',
     subject: '填写简短精炼的变更描述：\n',
     body: '填写更加详细的变更描述（可选）。使用 "|" 换行：\n',
     breaking: '列举非兼容性重大的变更（可选）：\n',
